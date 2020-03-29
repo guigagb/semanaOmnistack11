696af95febd347865b2d5f9b2f1cc28b0a6a74cd
guigagb <trindade.guilherme@outlook.com>
2020-03-28 23:16:40 -0300

    Finalizaçao do app mobile

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './src/routes';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+// View para container tipo div
+// Text para qualquer tipo de texto
+
+// Não existe herança de estilo
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
